46dc02e77938500c74cf5138650a4e4d076e4c21
Your Name <you@example.com>
2026-05-17 20:43:51 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "AWS CI/CD Pipeline Working!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
